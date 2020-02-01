439651d0757d913675c13d8a9abcc4c09f325309
mahanvyakti <44169656+mahanvyakti@users.noreply.github.com>
2020-02-01 13:16:14 +0530

    Initial Commit

---
+++ ExpenseTracker.py
@@ -0,0 +1,25 @@
+#importing libraries
+from tkinter import *
+from tkinter import scrolledtext
+from tkinter import messagebox
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
+import pandas as pd
+import cx_Oracle   
+import socket
+import datetime
+import bs4
+import operator
+
+def register():
+    register = Tk()
+    register.geometry('700x700+450+25')
+    start.title("Expenso !")
+    start.mainloop()
+
+#Start Page
+start = Tk()
+start.geometry('700x700+450+25')
+start.title("Expenso !")
+start.mainloop()
