a397e5ac66a0ba3ac3ce47d5a9574a0f4b7a269a
Aman Desai <amandesai01@gmail.com>
2020-10-02 14:13:46 +0530

    [UPDATE] Made Expense Tracker to Automatically Initialise DB if neccessary

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -1,8 +1,28 @@
 from tkinter import messagebox
 from Interface import profile, AddIncome, AddExpense, ExpvsTime
 import Login
-
+import db_functions
+import mysql.connector as mysql
+from dotenv import load_dotenv
+import os
 #Start Page
 
+load_dotenv()
+
+connection = mysql.connect(
+    host= os.getenv('MYSQL_HOST', default='localhost'),
+    user= os.getenv('MYSQL_USER', default='root'),
+    password= os.getenv('MYSQL_PASSWORD', default=''),
+    database= os.getenv('MYSQL_DATABASE', default='exptrack')
+)
+
+is_db_initialised = db_functions.check_if_db_initialised(connection)
+if not is_db_initialised:
+    queries = db_functions.get_initial_queries()
+    status = db_functions.execute_queries(queries)
+    if status:
+        print('DB INITIALISED SUCCESSFULLY')
+    else:
+        print('ERROR INITIALISING DB')
 register = Login.launch()
 
